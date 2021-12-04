07d2d47241aa693073de8af952f68b1fa01351bf
anthony <78743473+anthony-freitas@users.noreply.github.com>
2021-12-04 03:09:58 -0300

    checking types at runtime

--- lib/electral.ex
+++ lib/electral.ex
@@ -14,5 +14,5 @@ defmodule Electral do
   """
   def hello(), do: "hello"
   def hello(""), do: "hello"
-  def hello(name), do: "who is " <> name <> "?"
+  def hello(name) when is_binary(name), do: "who is " <> name <> "?"
 end
