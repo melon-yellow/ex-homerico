1f3a4e62f3bcee4734e6ac34a81c19a929388d78
anthony <78743473+anthony-freitas@users.noreply.github.com>
2021-12-08 23:09:29 -0300

    Update handlers.ex

--- lib/handlers.ex
+++ lib/handlers.ex
@@ -3,6 +3,6 @@ defmodule Unsafe.Handler do
 
   def bang!({:ok, return}), do: return
   def bang!({:error, reason}), do: throw reason
-  def bang!(_), do: throw "wrong return for handler"
+  def bang!(_), do: throw "wrong return for unsafe handler"
 
 end
